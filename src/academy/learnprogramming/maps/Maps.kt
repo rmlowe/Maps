911f16a0b5cf6e280263fb6826275b58b79330e7
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-13 13:48:26 +0800

    Sets in Kotlin

--- src/academy/learnprogramming/maps/Maps.kt
+++ src/academy/learnprogramming/maps/Maps.kt
@@ -3,6 +3,18 @@ package academy.learnprogramming.maps
 
 fun main(args: Array<String>) {
 
+    val setInts = setOf(10, 15, 19, 5, 3, -22)
+    println(setInts.plus(20))
+    println(setInts.plus(10))
+    println(setInts.minus(19))
+    println(setInts.minus(100))
+    println(setInts.average())
+    println(setInts.drop(3))
+
+    val mutableInts = mutableSetOf(1, 2, 3, 4, 5)
+    mutableInts.plus(10)
+    println(mutableInts)
+
     val immutableMap = mapOf<Int, Car>(1 to Car("green", "Toyota", 2015),
             2 to Car("red", "Ford", 2016),
             3 to Car("silver", "Honda", 2013))
